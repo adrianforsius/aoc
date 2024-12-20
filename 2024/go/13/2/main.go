a9ffd9035a117213b0d46a4c422a63a7c3224c3b
Adrian Forsius <adrianforsius@gmail.com>
2024-12-20 23:37:57 +0100

    Add day 13 start 2

--- 2024/go/13/2/main.go
+++ 2024/go/13/2/main.go
@@ -1,91 +1,73 @@
 package main
 
 import (
-	"fmt"
-	"slices"
 	"strconv"
 	"strings"
 )
 
-func Parse(in string) []int {
-	line := []int{}
-	for _, char := range strings.Split(in, "") {
-		num, _ := strconv.Atoi(char)
-		line = append(line, num)
-	}
-	return line
+type Coord struct {
+	X int
+	Y int
 }
 
-func toString(line []int) []string {
-	count := 0
-	display := []string{}
-	file := true
-	for _, num := range line {
-		for i := 0; i < num; i++ {
-			if file {
-				display = append(display, fmt.Sprint(count))
-			} else {
-				display = append(display, ".")
-			}
-		}
-		if file {
-			count++
-		}
-		file = !file
-	}
-	return display
+type Machine struct {
+	A     Coord
+	B     Coord
+	Prize Coord
 }
 
-func Day2(line []int) int {
-	blocks := toString(line)
+func Parse(in string) []Machine {
+	machines := []Machine{}
+	for _, machine := range strings.Split(in, "\n\n") {
+		lines := strings.Split(machine, "\n")
+		aLine := strings.Split(lines[0], "Button A: X+")
+		aStrs := strings.Split(aLine[1], ", Y+")
+		aX, _ := strconv.Atoi(aStrs[0])
+		aY, _ := strconv.Atoi(aStrs[1])
 
-	for i := len(blocks) - 1; i > 0; i-- {
-		if blocks[i] == "." {
-			continue
-		}
+		bLine := strings.Split(lines[1], "Button B: X+")
+		bStrs := strings.Split(bLine[1], ", Y+")
+		bX, _ := strconv.Atoi(bStrs[0])
+		bY, _ := strconv.Atoi(bStrs[1])
 
-		char := blocks[i]
-		fileFrom := i
-		fileTo := i
-		for fileTo > 0 && blocks[fileTo] == char {
-			fileTo--
-		}
-		i = fileTo + 1
-		fileSize := fileFrom - fileTo
+		pLine := strings.Split(lines[2], "Prize: X=")
+		pStrs := strings.Split(pLine[1], ", Y=")
+		pX, _ := strconv.Atoi(pStrs[0])
+		pY, _ := strconv.Atoi(pStrs[1])
 
-		blockFrom := slices.IndexFunc(blocks, func(s string) bool {
-			return s != "."
+		machines = append(machines, Machine{
+			A:     Coord{X: aX, Y: aY},
+			B:     Coord{X: bX, Y: bY},
+			Prize: Coord{X: pX + 1_000_000_000_0000, Y: pY + 1_000_000_000_0000},
 		})
-		blockTo := blockFrom
-		for k := blockFrom; k < len(blocks)-1; k++ {
-			if blocks[k] == "." {
-				blockTo++
-			} else {
-				blockFrom = k
-				blockTo = k
-			}
-			if blockTo-blockFrom == fileSize {
-				break
-			}
+	}
+	return machines
+}
+
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func Day2(machines []Machine) int {
+	sum := 0
+	for _, m := range machines {
+		d := m.A.X*m.B.Y - m.A.Y*m.B.X
+
+		d1 := m.Prize.X*m.B.Y - m.Prize.Y*m.B.X
+		d2 := m.Prize.Y*m.A.X - m.Prize.X*m.A.Y
+
+		a, b := 0, 0
+		if d1%d == 0 && d2%d == 0 {
+			a = d1 / d
+			b = d2 / d
 		}
-		if fileTo < blockTo {
+		if a <= 0 || b <= 0 {
 			continue
 		}
-
-		if blockTo-blockFrom == fileSize {
-			for f := 0; f < fileSize; f++ {
-				blocks[blockFrom+1] = blocks[fileTo+1]
-				blocks[fileTo+1] = "."
-				blockFrom++
-				fileTo++
-			}
-		}
-	}
-
-	sum := 0
-	for i, v := range blocks {
-		val, _ := strconv.Atoi(v)
-		sum += i * val
+		sum += (3 * a) + b
 	}
 
 	return sum
