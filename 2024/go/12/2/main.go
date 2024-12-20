45d0186f08813e54317589d97d357b538c24f84f
Adrian Forsius <adrianforsius@gmail.com>
2024-12-20 20:06:31 +0100

    Add day 12 give up part 2

--- 2024/go/12/2/main.go
+++ 2024/go/12/2/main.go
@@ -2,90 +2,90 @@ package main
 
 import (
 	"fmt"
-	"slices"
-	"strconv"
 	"strings"
 )
 
-func Parse(in string) []int {
-	line := []int{}
-	for _, char := range strings.Split(in, "") {
-		num, _ := strconv.Atoi(char)
-		line = append(line, num)
-	}
-	return line
+type Coord struct {
+	X   int
+	Y   int
+	Val string
 }
 
-func toString(line []int) []string {
-	count := 0
-	display := []string{}
-	file := true
-	for _, num := range line {
-		for i := 0; i < num; i++ {
-			if file {
-				display = append(display, fmt.Sprint(count))
-			} else {
-				display = append(display, ".")
-			}
-		}
-		if file {
-			count++
-		}
-		file = !file
-	}
-	return display
+type Grid struct {
+	grid    [][]Coord
+	visited map[Coord]int
 }
 
-func Day2(line []int) int {
-	blocks := toString(line)
+var dir = []Coord{
+	{X: 1, Y: 0},
+	{X: -1, Y: 0},
+	{X: 0, Y: 1},
+	{X: 0, Y: -1},
+}
 
-	for i := len(blocks) - 1; i > 0; i-- {
-		if blocks[i] == "." {
-			continue
-		}
-
-		char := blocks[i]
-		fileFrom := i
-		fileTo := i
-		for fileTo > 0 && blocks[fileTo] == char {
-			fileTo--
-		}
-		i = fileTo + 1
-		fileSize := fileFrom - fileTo
-
-		blockFrom := slices.IndexFunc(blocks, func(s string) bool {
-			return s != "."
-		})
-		blockTo := blockFrom
-		for k := blockFrom; k < len(blocks)-1; k++ {
-			if blocks[k] == "." {
-				blockTo++
-			} else {
-				blockFrom = k
-				blockTo = k
-			}
-			if blockTo-blockFrom == fileSize {
-				break
-			}
-		}
-		if fileTo < blockTo {
-			continue
-		}
-
-		if blockTo-blockFrom == fileSize {
-			for f := 0; f < fileSize; f++ {
-				blocks[blockFrom+1] = blocks[fileTo+1]
-				blocks[fileTo+1] = "."
-				blockFrom++
-				fileTo++
-			}
-		}
+func (g Grid) valid(coord Coord) bool {
+	if coord.X < 0 || coord.X >= len(g.grid[0]) || coord.Y < 0 || coord.Y >= len(g.grid) {
+		return false
 	}
+	if coord.Val != g.grid[coord.Y][coord.X].Val {
+		return false
+	}
+	return true
+}
 
+type Lot struct {
+	Sides map[Coord]int
+	Size  int
+}
+
+func (g Grid) Walk(coord Coord) Lot {
+	if !g.valid(coord) {
+		return Lot{Parameter: 1}
+	}
+	if _, ok := g.visited[coord]; ok {
+		return Lot{}
+	}
+	g.visited[coord] = 1
+	// fmt.Println("coord", coord)
+
+	// sum := 0
+	// sum += g.Walk(Coord{X: coord.X + dir[0][0], Y: coord.Y + dir[0][1], Val: coord.Val})
+	// sum += g.Walk(Coord{X: coord.X + dir[1][0], Y: coord.Y + dir[1][1], Val: coord.Val})
+	// sum += g.Walk(Coord{X: coord.X + dir[2][0], Y: coord.Y + dir[2][1], Val: coord.Val})
+	// sum += g.Walk(Coord{X: coord.X + dir[3][0], Y: coord.Y + dir[3][1], Val: coord.Val})
+	up := g.Walk(Coord{X: coord.X + dir[0].X, Y: coord.Y + dir[0].Y, Val: coord.Val})
+	right := g.Walk(Coord{X: coord.X + dir[1].X, Y: coord.Y + dir[1].Y, Val: coord.Val})
+	down := g.Walk(Coord{X: coord.X + dir[2].X, Y: coord.Y + dir[2].Y, Val: coord.Val})
+	left := g.Walk(Coord{X: coord.X + dir[3].X, Y: coord.Y + dir[3].Y, Val: coord.Val})
+	return Lot{
+		Parameter: up.Parameter + right.Parameter + down.Parameter + left.Parameter,
+		Size:      up.Size + right.Size + down.Size + left.Size + 1,
+	}
+}
+
+func Parse(in string) Grid {
+	grid := [][]Coord{}
+	for y, line := range strings.Split(in, "\n") {
+		chars := strings.Split(line, "")
+		cs := []Coord{}
+		for x, char := range chars {
+			cs = append(cs, Coord{x, y, char})
+		}
+		grid = append(grid, cs)
+	}
+	return Grid{grid, make(map[Coord]int, 0)}
+}
+
+func Day2(grid Grid) int {
 	sum := 0
-	for i, v := range blocks {
-		val, _ := strconv.Atoi(v)
-		sum += i * val
+	for _, row := range grid.grid {
+		for _, coord := range row {
+			if _, ok := grid.visited[coord]; !ok {
+				lot := grid.Walk(coord)
+				fmt.Println("lot", lot)
+				sum += lot.Parameter * lot.Size
+			}
+		}
 	}
 
 	return sum
