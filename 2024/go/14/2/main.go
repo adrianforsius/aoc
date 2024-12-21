68cf63619e5f0173036f8b2b3ba8c8cd6470ca8f
Adrian Forsius <adrianforsius@gmail.com>
2024-12-21 17:53:11 +0100

    Add day 14 star 2

--- 2024/go/14/2/main.go
+++ 2024/go/14/2/main.go
@@ -2,91 +2,95 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strconv"
 	"strings"
 )
 
-func Parse(in string) []int {
-	line := []int{}
-	for _, char := range strings.Split(in, "") {
-		num, _ := strconv.Atoi(char)
-		line = append(line, num)
-	}
-	return line
+type Robot struct {
+	X int
+	Y int
+
+	XM int
+	YM int
 }
 
-func toString(line []int) []string {
-	count := 0
-	display := []string{}
-	file := true
-	for _, num := range line {
-		for i := 0; i < num; i++ {
-			if file {
-				display = append(display, fmt.Sprint(count))
-			} else {
-				display = append(display, ".")
-			}
-		}
-		if file {
-			count++
-		}
-		file = !file
-	}
-	return display
+type Coord struct {
+	X int
+	Y int
 }
 
-func Day2(line []int) int {
-	blocks := toString(line)
+func Parse(in string) []Robot {
+	robots := []Robot{}
+	for _, line := range strings.Split(in, "\n") {
+		aLine := strings.Split(line, "p=")
+		midLine := strings.Split(aLine[1], " v=")
+		pParts := strings.Split(midLine[0], ",")
+		pX, _ := strconv.Atoi(pParts[0])
+		pY, _ := strconv.Atoi(pParts[1])
+		vParts := strings.Split(midLine[1], ",")
+		vX, _ := strconv.Atoi(vParts[0])
+		vY, _ := strconv.Atoi(vParts[1])
 
-	for i := len(blocks) - 1; i > 0; i-- {
-		if blocks[i] == "." {
-			continue
-		}
-
-		char := blocks[i]
-		fileFrom := i
-		fileTo := i
-		for fileTo > 0 && blocks[fileTo] == char {
-			fileTo--
-		}
-		i = fileTo + 1
-		fileSize := fileFrom - fileTo
-
-		blockFrom := slices.IndexFunc(blocks, func(s string) bool {
-			return s != "."
+		robots = append(robots, Robot{
+			X:  pX,
+			Y:  pY,
+			XM: vX,
+			YM: vY,
 		})
-		blockTo := blockFrom
-		for k := blockFrom; k < len(blocks)-1; k++ {
-			if blocks[k] == "." {
-				blockTo++
-			} else {
-				blockFrom = k
-				blockTo = k
-			}
-			if blockTo-blockFrom == fileSize {
-				break
-			}
-		}
-		if fileTo < blockTo {
-			continue
-		}
+	}
+	return robots
+}
 
-		if blockTo-blockFrom == fileSize {
-			for f := 0; f < fileSize; f++ {
-				blocks[blockFrom+1] = blocks[fileTo+1]
-				blocks[fileTo+1] = "."
-				blockFrom++
-				fileTo++
-			}
-		}
+func Day2(r []Robot, height, width int) int {
+	coords := map[Coord][]Robot{}
+	for _, v := range r {
+		coords[Coord{v.X, v.Y}] = []Robot{v}
 	}
 
-	sum := 0
-	for i, v := range blocks {
-		val, _ := strconv.Atoi(v)
-		sum += i * val
-	}
+	for i := 1; i < 10_000; i++ {
+		rs := map[Coord][]Robot{}
+		unique := true
+		for _, coord := range coords {
+			for _, robot := range coord {
+				x := robot.X + robot.XM
+				y := robot.Y + robot.YM
 
-	return sum
+				dx := x % width
+				if dx < 0 {
+					dx = width + dx
+				}
+				dy := y % height
+				if dy < 0 {
+					dy = height + dy
+				}
+
+				// halfWidth := width / 2
+				// halfHeight := height / 2
+				robot.X = dx
+				robot.Y = dy
+				if _, ok := rs[Coord{robot.X, robot.Y}]; ok {
+					rs[Coord{robot.X, robot.Y}] = append(rs[Coord{robot.X, robot.Y}], robot)
+					unique = false
+				} else {
+					rs[Coord{robot.X, robot.Y}] = []Robot{robot}
+				}
+			}
+			coords = rs
+		}
+		if unique {
+			fmt.Println("count", i)
+			for y := 0; y < height; y++ {
+				for x := 0; x < width; x++ {
+					if _, ok := rs[Coord{x, y}]; ok {
+						fmt.Print("#")
+					} else {
+						fmt.Print(".")
+					}
+				}
+				fmt.Println()
+			}
+			return 0
+		}
+	}
+	return 0
 }
--- 2024/go/14/2/main_test.go
+++ 2024/go/14/2/main_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDay2Example(t *testing.T) {
-	const expected = 2858
-	assert.Equal(t, expected, Day2(Parse(exampleInput)))
-}
+// func TestDay2Example(t *testing.T) {
+// 	const expected = 2858
+// 	assert.Equal(t, expected, Day2(Parse(exampleInput)))
+// }
 
 func TestDay2(t *testing.T) {
 	const expected = 0
-	assert.Equal(t, expected, Day2(Parse(input)))
+	assert.Equal(t, expected, Day2(Parse(input), 103, 101))
 }
